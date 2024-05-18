244468ab7e333592955e152a82353e626bd7fe9d
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-05-18 17:25:07 +0530

    components setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import VideoPlayer from "./components/VideoPlayer";
+import Notes from "./components/Notes";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [currentVideoId, setCurrentVideoId] = useState("O6P86uwfdR0");
+
+  const [videoInfo, setVideoInfo] = useState({ title: "", description: "" });
+
+  useEffect(() => {
+    const fetchVideoDetails = async () => {
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${currentVideoId}&key=AIzaSyBnvFfZaXf6z6TFFRU5e9lGXcd-nJ4xSCM`
+        );
+        const videoData = response.data.items[0].snippet;
+        setVideoInfo({
+          title: videoData.title,
+          description: videoData.description,
+        });
+      } catch (error) {
+        console.error("Error in fetching video details:", error);
+      }
+    };
+
+    fetchVideoDetails();
+  }, [currentVideoId]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>YouTube Video Note Taker</h1>
+      <input
+        type="text"
+        value={currentVideoId}
+        onChange={(e) => setCurrentVideoId(e.target.value)}
+        placeholder="Enter any youtube video ID..."
+      />
+      <VideoPlayer />
+      <div>
+        <h2>{videoInfo.title}</h2>
+        <p>{videoInfo.description}</p>
+      </div>
+      <Notes />
     </div>
   );
-}
+};
 
 export default App;
